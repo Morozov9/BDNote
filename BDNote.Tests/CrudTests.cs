6c5913b87c7a5e2f98d27d65aa21d8c208fb658e
Leonid <ldruzovskiy920@gmail.com>
2026-05-03 18:35:52 +0500

    Initial commit

---
+++ BDNote.Tests/CrudTests.cs
@@ -0,0 +1,69 @@
+﻿namespace BDNote.Tests;
+
+public class CrudTests
+{
+    [Fact]
+    public async Task TestCreate()
+    {
+        await using var db = new DataContext();
+        await db.Database.EnsureCreatedAsync(); 
+
+        // Act
+        var note = await Crud.Create("Простая заметка", DateTimeOffset.Now);
+
+        // Assert
+        Assert.True(note.Id > 0);
+        Assert.Equal("Простая заметка", note.Text);
+    }
+    
+    [Fact]
+    public async Task TestReadById()
+    {
+        await using var db = new DataContext();
+        await db.Database.EnsureCreatedAsync(); 
+
+        // Arrange
+        var note = await Crud.Create("Найти меня", DateTimeOffset.Now);
+
+        // Act
+        var found = await Crud.Read(note.Id);
+
+        // Assert
+        Assert.NotNull(found);
+        Assert.Equal(note.Id, found.Id);
+    }
+
+    [Fact]
+    public async Task TestUpdate()
+    {
+        await using var db = new DataContext();
+        await db.Database.EnsureCreatedAsync(); 
+
+        // Arrange
+        var note = await Crud.Create("Старый текст", DateTimeOffset.Now);
+
+        // Act
+        await Crud.Update(note, "Новый текст", DateTimeOffset.Now);
+
+        // Assert
+        var updated = await Crud.Read(note.Id);
+        Assert.Equal("Новый текст", updated?.Text);
+    }
+
+    [Fact]
+    public async Task TestDelete()
+    {
+        await using var db = new DataContext();
+        await db.Database.EnsureCreatedAsync(); 
+
+        // Arrange
+        var note = await Crud.Create("Удали меня", DateTimeOffset.Now);
+
+        // Act
+        await Crud.Delete(note);
+
+        // Assert
+        var deleted = await Crud.Read(note.Id);
+        Assert.Null(deleted);
+    }
+}
---
+++ BDNote/Crud.cs
@@ -0,0 +1,73 @@
+namespace BDNote;
+
+using Microsoft.EntityFrameworkCore;
+
+public class Crud
+{
+    /// <summary>
+    /// Создаёт новую заметку и сохраняет его в БД.
+    /// </summary>
+    /// <returns>
+    /// Сущность новой заметки. После сохранения в БД её свойство <see cref="Student.Id"/>
+    /// будет содержать реальный ID из СУБД (не 0).
+    /// </returns>
+    public static async Task<Note> Create(string text, DateTimeOffset createdAt, CancellationToken ct = default)
+    {
+        await using var db = new DataContext();
+        var note = new Note
+        {
+            Text = text,
+            CreatedAt = createdAt
+        };
+        
+        db.Notes.Add(note);
+        await db.SaveChangesAsync(ct);
+        return note;
+    }
+
+    /// <summary>
+    /// Получает список заметок с поиском по частичному совпадению текста.
+    /// </summary>
+    public static async Task<List<Note>> Read(string searchText, CancellationToken ct = default)
+    {
+        await using var db = new DataContext();
+        return await db.Notes
+            .Where(x => EF.Functions.Like(x.Text, $"%{searchText}%"))
+            .AsNoTracking() 
+            .ToListAsync(ct);
+    }
+
+    /// <summary>
+    /// Ищет конкретную заметку по его ID.
+    /// </summary>
+    /// <returns>
+    /// Сущность найденной заметки или <see langword="null"/> если такого нет.
+    /// </returns>
+    public static async Task<Note?> Read(int id, CancellationToken ct = default)
+    {
+        await using var db = new DataContext();
+        return await db.Notes.FirstOrDefaultAsync(x => x.Id == id, ct);
+    }
+
+    /// <summary>
+    /// Обновляет сущность заметки в БД. Меняет данные в той же сущности, не создаёт новый инстанс.
+    /// </summary>
+    public static async Task Update(Note note, string text, DateTimeOffset createdAt, CancellationToken ct = default)
+    {
+        await using var db = new DataContext();
+        note.Text = text;
+        note.CreatedAt = createdAt;
+        db.Notes.Update(note);
+        await db.SaveChangesAsync(ct);
+    }
+
+    /// <summary>
+    /// Удаляет сущность заметки из БД.
+    /// </summary>
+    public static async Task Delete(Note note, CancellationToken ct = default)
+    {
+        await using var db = new DataContext();
+        db.Notes.Remove(note);
+        await db.SaveChangesAsync(ct);
+    }
+}
